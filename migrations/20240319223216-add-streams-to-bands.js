c8ee1da6ea2dc0b5580c865f69154793bff005fd
wguadalupe <w.guadalupe78@gmail.com>
2024-03-19 19:02:44 -0400

    safety commit

---
+++ migrations/20240319223216-add-streams-to-bands.js
@@ -0,0 +1,11 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('bands', 'recommendation', {
+        type: DataTypes.STRING
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('bands', 'recommendation')
+  }
+}                
